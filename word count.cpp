20ec0aa54c19121623581909ae7648c71099305c
Anuj Kumar <93114862+anuj050801@users.noreply.github.com>
2023-09-23 21:27:24 +0530

    Add files via upload

---
+++ word count.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#include <fstream>
+
+#include <string>
+
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    ifstream inputFile("test.txt");
+
+    if (inputFile.is_open())
+    {
+        string line;
+        int wordCount = 0;
+
+        while (getline(inputFile, line))
+        {
+            stringstream ss(line);
+            string word;
+
+            while (ss >> word)
+            {
+                wordCount++;
+            }
+        }
+
+        inputFile.close();
+
+        cout << "Number of words in the said file: " << wordCount << endl;
+    }
+    else
+    {
+        cout << "Failed to open the file." << endl;
+    }
+
+    return 0;
+}
